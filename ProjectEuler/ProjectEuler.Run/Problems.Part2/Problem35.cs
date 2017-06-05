28303f80effc674da4dc435b908e96fc6942a477
herara-ofnir3 <herara130@gmail.com>
2017-06-05 19:23:28 +0900

    Problem35を追加。(途中)

---
+++ ProjectEuler/ProjectEuler.Run/Problems.Part2/Problem35.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler.Run.Problems.Part2
+{
+	/// <summary>
+	/// Problem 35 「巡回素数」
+	/// 197は巡回素数と呼ばれる. 桁を回転させたときに得られる数 197, 971, 719 が全て素数だからである.
+	/// 
+	/// 100未満には巡回素数が13個ある: 2, 3, 5, 7, 11, 13, 17, 31, 37, 71, 73, 79, および97である.
+	/// 
+	/// 100万未満の巡回素数はいくつあるか?
+	/// </summary>
+	public class Problem35 : Problem
+	{
+		public override string Run()
+		{
+			var l = Primes(100).ToList();
+
+			var answer = string.Empty;
+			return answer.ToString();
+		}
+
+		static IEnumerable<IEnumerable<int>> Circulars(IEnumerable<int> digits)
+		{
+			var count = digits.Count();
+			foreach (var i in Enumerable.Range(0, count))
+			{
+				var left = digits.Skip(i).Take(count - i);
+				var right = digits.Take(i);
+				yield return left.Concat(right);
+			}
+		}
+
+		static IEnumerable<int> Primes(int max)
+		{
+			var map = new Dictionary<int, bool>();
+			foreach (var l in Enumerable.Range(2, max - 1))
+				map.Add(l, true);
+
+			var head = 0;
+			var sqrt = Math.Sqrt(max);
+			
+			while (head <= sqrt)
+			{
+				head = map
+					.Where(x => x.Value)
+					.First(x => head < x.Key)
+					.Key;
+
+				yield return head;
+
+				var removes = Multiples(head)
+					.Skip(1)
+					.TakeWhile(x => x <= max);
+
+				foreach (var r in removes)
+					map[r] = false;
+			}
+
+			foreach (var m in map.Where(m => head < m.Key && m.Value))
+				yield return m.Key;
+		}
+
+		static IEnumerable<int> Multiples(int n)
+		{
+			for (var m = n; ; m += n)
+			{
+				yield return m;
+			}
+		}
+	}
+}
