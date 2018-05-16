ce55d9b6253e736df64802528d69a2f171b8d580
herara-ofnir3 <herara130@gmail.com>
2018-05-16 10:15:06 +0900

    Problem4を修正。

--- ProjectEuler-Haskell/problem4.hs
+++ ProjectEuler-Haskell/problem4.hs
@@ -4,12 +4,11 @@ digits n
 
 isPalindromic n = (digits n) == (reverse (digits n))
 
-numbersBy d = [(10 ^ (d - 1))..(10 ^ d - 1)]
-numbers = numbersBy 3
-products = numbers >>= (\ x -> map (\ y -> x * y) numbers)
-
-problem4 = maximum (filter (\ p -> isPalindromic p) products)
---problem4 = digits 920394020
+d = 3
+problem4 = maximum [ p | x <- [(10 ^ (d - 1))..(10 ^ d - 1)], 
+                         y <- [x..(10 ^ d - 1)], 
+                         let p = x * y, 
+                         isPalindromic p ]
 
 main = do
     print problem4
